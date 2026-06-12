06c9386a95981aa3c443bb717996c0e23c58941f
prettifyAditya <adityashukla.prettifycreative@gmail.com>
2026-06-12 15:42:06 +0530

    feat: added components from deepak branch

---
+++ components/frontendcomponent/organisms/TabContainer.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useState } from "react";
+import SlideBtn from "../atoms/SlideBtn";
+import ResourceCard from "../molecules/ResourceCard";
+import "@/uploads/styles/component/component.css";
+
+
+const TabContainer = ({ data, card }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <div className="container tab-container">
+      <div className="row">
+        <ul className="tabs">
+          {[{ category: "All" }, ...data]?.map(({ category }, i) => {
+            const isActive = activeTab === i;
+            return (
+              <li
+                onClick={() => setActiveTab(i)}
+                key={i}
+                className={`tab ${isActive ? "active" : ""}`}
+              >
+                {category}
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="swiper-nav group primary-border square">
+          <SlideBtn className={`resource-button-prev-${activeTab}`} />
+          <SlideBtn className={`resource-button-next-${activeTab}`} />
+        </div>
+      </div>
+
+      {[
+        {
+          category: "All",
+          list: data.flatMap(({ list }) => {
+            return list;
+          }),
+        },
+        ...data,
+      ]?.map(({ category, list }, idx) => {
+        const isActive = activeTab === idx;
+        return (
+          <div key={idx} className={`content ${isActive ? "active" : ""}`}>
+            <Swiper
+              navigation={{
+                nextEl: `.resource-button-next-${idx}`,
+                prevEl: `.resource-button-prev-${idx}`,
+              }}
+              modules={[Navigation]}
+              slidesPerView={4}
+              spaceBetween={20}
+            >
+              {list.map((item, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    {card === "resource" ? (
+                      <ResourceCard category={category} {...item} />
+                    ) : (
+                      ""
+                    )}
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default TabContainer;
