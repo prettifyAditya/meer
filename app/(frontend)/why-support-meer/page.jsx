8f09b771fcc5c69b4f34377a88205f324dddd8de
Keshav Sharma <keshavsharma.prettifycreative@gmail.com>
2026-06-13 11:13:33 +0530

    started working on why support meer page

---
+++ app/(frontend)/why-support-meer/page.jsx
@@ -0,0 +1,12 @@
+import WhySupportMeerPage from '@/components/frontendcomponent/pages/why-support-meer/indes'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <WhySupportMeerPage />
+        </>
+    )
+}
+
+export default page
---
+++ components/frontendcomponent/pages/why-support-meer/indes.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import HeroSection from '../../molecules/HeroSection'
+
+const WhySupportMeerPage = () => {
+    return (
+        <>
+            <HeroSection
+                classname="how_works_hero"
+                mediaSrc="/image/how-works/how_banner.jpg"
+                tagline="How It Works"
+                heading="Surface Radiative Thermal Management (SRTM)"
+            />
+        </>
+    )
+}
+
+export default WhySupportMeerPage
