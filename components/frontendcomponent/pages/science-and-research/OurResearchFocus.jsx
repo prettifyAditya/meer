d77b1efd4ffa72138b4031a6294a311b011a81f9
Keshav Sharma <keshavsharma.prettifycreative@gmail.com>
2026-06-16 18:29:36 +0530

    Completed CROI page

--- components/frontendcomponent/pages/science-and-research/OurResearchFocus.jsx
+++ components/frontendcomponent/pages/science-and-research/OurResearchFocus.jsx
@@ -1,7 +1,5 @@
 "use client";
 import React from 'react'
-import Image from 'next/image'
-import Button from '../../atoms/Button'
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SlideBtn from '../../atoms/SlideBtn';
